b11d6ad847afe2d6c066c1761c89681cd8e20257
Jacek Podkanski <ruby.object@googlemail.com>
2013-11-09 16:12:09 +0000

    preparing for adding content

---
+++ app/views/home.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Chris Beard Food Photography</title>
+<link type="text/css" rel="stylesheet" media="all" href="style.css">
+</head>
+<body>
+        <h1>Chris Beard Food Photography</h1>
+</body>
+</html>
