b7648d34bc781aee0978bc51eae9cdf908c4aef8
Jacek <ruby.object@googlemail.com>
2013-11-09 17:14:14 +0000

    added layout

---
+++ app/views/about.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.base')
+
+
+@section('body')
+        @include('navbar')
+<h1>Hurray!</h1>
+<p>We have a template!</p>
+@stop
--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -7,11 +7,6 @@
 </head>
 <body>
         <h1>Chris Beard Food Photography</h1>
-        <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/work">Work</a></li>
-          <li><a href="/contact">Contact</a></li>
-        </ul>
+        @include('navbar')
 </body>
 </html>
