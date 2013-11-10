6c49a74eccf92e29fd75f636f80711a462e911b2
Jacek Podkanski <ruby.object@googlemail.com>
2013-11-10 21:05:25 +0000

    changed indentation

--- app/views/layout/base.blade.php
+++ app/views/layout/base.blade.php
@@ -1,36 +1,38 @@
 <!doctype html>
 <html lang="en">
 <head>
-	  <meta charset="UTF-8">
-	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	  <title></title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title></title>
 
-	  @section('head')
-	  <link rel="stylesheet" href="/stylesheets/style.css">
-	  <script src="/javascripts/modernizr-full.js"></script>
-	  @show
+  @section('head')
+  <link rel="stylesheet" href="/stylesheets/style.css">
+  <!-- query external stylesheet -->
+  <link rel="stylesheet" media="screen and (min-width:320px) and (max-width:480px)" href="/stylesheets/mobile.css" >
+  <script src="/javascripts/modernizr-full.js"></script>
+  @show
 </head>
 <body>
-	  @include('layout.navbar')
+  @include('layout.navbar')
 
-	  @yield('body')
+  @yield('body')
 
-	  <script>
-	  Modernizr.load([
-		  {
-			load: '//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.js',
-			complete: function () {
-				  if ( !window.jQuery ) {
-					  Modernizr.load('js/libs/jquery-1.7.1.min.js');
-				  }
-			  }
-		  },
-		  {
-			  // This will wait for the fallback to load and
-			  // execute if it needs to.
-			load: 'needs-jQuery.js'
-		  }
-	  ]);
-      </script>
+  <script>
+    Modernizr.load([
+	{
+	    load: '//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.js',
+	    complete: function () {
+	        if ( !window.jQuery ) {
+	            Modernizr.load('js/libs/jquery-1.7.1.min.js');
+	        }
+	    }
+	},
+	{
+	    // This will wait for the fallback to load and
+	    // execute if it needs to.
+	    load: 'needs-jQuery.js'
+	}
+    ]);
+  </script>
 </body>
 </html>
