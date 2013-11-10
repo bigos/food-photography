e5735d6254d42fabad59f90f8fd0ffca22a436fc
Jacek Podkanski <ruby.object@googlemail.com>
2013-11-10 21:52:03 +0000

    moved modernizr

--- app/views/layout/base.blade.php
+++ app/views/layout/base.blade.php
@@ -17,22 +17,6 @@
 
   @yield('body')
 
-  <script>
-    Modernizr.load([
-	{
-	    load: '//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.js',
-	    complete: function () {
-	        if ( !window.jQuery ) {
-	            Modernizr.load('js/libs/jquery-1.7.1.min.js');
-	        }
-	    }
-	},
-	{
-	    // This will wait for the fallback to load and
-	    // execute if it needs to.
-	    load: 'needs-jQuery.js'
-	}
-    ]);
-  </script>
+  @include('layout.modernizr')
 </body>
 </html>
